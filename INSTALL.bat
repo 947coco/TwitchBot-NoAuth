dfbc248246b6a6e184419694cf97e39afff0fcd7
947Coco <None969@protonmail.com>
2025-10-03 09:30:18 +0200

    translated to english and refactor INSTALL.bat

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,16 +1,18 @@
 @echo off
 python --version 
 if %ERRORLEVEL% == 0 (
-    echo python est deja installe
+    echo python is already installed
 ) else (
     winget install Python 3.13 --accept-package-agreements
 )
-python -m venv venv
-call .\venv\Scripts\activate.bat
+set PATH_VENV=%USERPROFILE%\Documents\TwitchBot
+set PATH_START_FILE=%USERPROFILE%\Documents\START_TWITCH_BOT.bat
+python -m venv %PATH_VENV%
+call %PATH_VENV%\Scripts\activate.bat
 pip install pyttsx3
-echo @echo off > LANCER.bat
-echo call .\venv\Scripts\activate.bat >> LANCER.bat
-echo python .\scripts\main.py >> LANCER.bat
-echo pause >> LANCER.bat
-start LANCER.bat
+echo @echo off > %PATH_START_FILE%
+echo call %PATH_VENV%\Scripts\activate.bat >> %PATH_START_FILE%
+echo python .\scripts\main.py >> %PATH_START_FILE%
+echo pause >> %PATH_START_FILE%
+start %PATH_START_FILE%
 del "%0"
--- scripts/VariablesCheck.py
+++ scripts/VariablesCheck.py
@@ -5,26 +5,28 @@ class Variables():
             self.raw = file.readlines()
 
     def FindContent(self, ligne: str):
+        """Return the variable between quotes"""
         indexs = []
         for i, char in enumerate(ligne):
             if char == '"': indexs.append(i)
         return ligne[indexs[0]+1:indexs[1]]
 
     def GetVariables(self):
+        word = ""
         for line in self.raw:
-            if "PseudoTwitch" in line: USERNAME = self.FindContent(line)
-            if "MotsBannis" in line: BANWORDS = self.FindContent(line).split(" ")
-            if "DélaisVérification" in line: DELAIS = self.FindContent(line)
-            if "FormatRéponse" in line: FORMAT = self.FindContent(line)
-            if "RaccourciToutArrêter" in line: SHORTCUT = self.FindContent(line)
-            if "IaChoisi" in line: AI = self.FindContent(line)
+            if "Twitch username" in line: USERNAME = self.FindContent(line)
+            elif "Delay check" in line: DELAY = self.FindContent(line)
+            elif "Ban words" in line: BANWORDS = self.FindContent(line).split(" ")
+            elif "Anwser format" in line: FORMAT = self.FindContent(line)
+            elif "Shortcut to stop everything" in line: SHORTCUT = self.FindContent(line)
+            elif "Number of caracters max" in line: CHAR_MAX = self.FindContent(line)
 
         if '' in BANWORDS: BANWORDS.remove('')
         if ' ' in BANWORDS: BANWORDS.remove(' ')
 
-        MessageErreurUSERNAME = "Aucun pseudo twitch n'a été détécté, merci de modifier le fichier config.txt"
-        MessageErreurSHORTCUT = "Le raccourci clavier doit contenir Ctrl, Alt ou Shif, merci de modifier le fichier config.txt"
-        assert len(USERNAME) > 0 , MessageErreurUSERNAME
-        assert "ctrl" in SHORTCUT or "alt" in SHORTCUT or "shift" in SHORTCUT, MessageErreurSHORTCUT
+        ERROR_USERNAME = "Aucun pseudo twitch n'a été détécté, merci de modifier le fichier config.txt"
+        ERROR_SHORTCUT = "Le raccourci clavier doit contenir Ctrl, Alt ou Shif, merci de modifier le fichier config.txt"
+        assert len(USERNAME) > 0 , ERROR_USERNAME
+        assert any(key in SHORTCUT for key in ("ctrl", "alt", "shift")), ERROR_SHORTCUT
         
-        return USERNAME, BANWORDS,  DELAIS, FORMAT, SHORTCUT
+        return USERNAME, BANWORDS, DELAY, FORMAT, SHORTCUT, CHAR_MAX
